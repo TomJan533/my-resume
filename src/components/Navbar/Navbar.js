c6567e677c85b6b5c8fc8c69ac9f97f243790fd3
TomJan533 <tomasz.jankiewicz+github@sofomo.com>
2024-08-10 20:50:45 +0200

    scrolled navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 50) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  let navbarClasses = ['navbar'];
+  if (scrolled) {
+    navbarClasses.push('scrolled');
+  }
+
   return (
-    <nav className="navbar">
+    <nav className={navbarClasses.join(' ')}>
       <ul className="nav-list">
         <li className="nav-item"><a href="#home">Home</a></li>
         <li className="nav-item"><a href="#about">About</a></li>
